73372574efa21b5958b36fc9110a35804008b08d
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-04-15 13:16:41 -0300

    Ajustando a estrutura e inicio de script dando error

--- script_leitor.py
+++ script_leitor.py
@@ -1,1 +1,16 @@
 import os
+import pdfplumber
+import shutil
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+pasta_pdfs = os.path.join(base_dir, "PDFs")
+os.makedirs(pasta_pdfs, exist_ok=True)
+
+pasta_saida = os.path.join(base_dir, "txt_extraidos")
+os.makedirs(pasta_saida, exist_ok=True)
+
+pdfs = [f for f in os.listdir(pasta_pdfs) if f.lower().endswith(".pdf")]
+
+if not pdfs:
+    print("🚫 Nenhum PDF encontrado na pasta 'PDFs'.")
